783030f93a91ab62b39778f375ff402390695e0b
Hogeun Yoo <ho93un+git@gmail.com>
2021-06-11 01:08:02 +0900

    리팩토링전

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 from scipy.io import wavfile
-from scipy.signal import lfilter, butter
+from scipy.signal import lfilter, butter, hilbert
 
-FAST_MODE = False
+FIRST_SIGNAL_SAMPLE_RATE, FIRST_ORIGINAL_SIGNAL = wavfile.read('./wavFiles/ho_id_1.wav')
+SECOND_SIGNAL_SAMPLE_RATE, SECOND_ORIGINAL_SIGNAL = wavfile.read('./wavFiles/ok_id_0.wav')
 
-FIRST_SIGNAL_SAMPLE_RATE, FIRST_ORIGINAL_SIGNAL = wavfile.read('./wavFiles/card_su.wav')
-SECOND_SIGNAL_SAMPLE_RATE, SECOND_ORIGINAL_SIGNAL = wavfile.read('./wavFiles/card_ho.wav')
+
+def get_envelop(signal):
+    analytic_signal = hilbert(signal)
+    amplitude_envelope = np.abs(analytic_signal)
+    return amplitude_envelope
+
+
+def show_plot(first_signal, second_signal):
+    plt.plot(first_signal, color='red')
+    plt.plot(second_signal, color='blue')
+    plt.axis('tight')
+    plt.xlabel('samples')
+    plt.ylabel('amplitude')
+    plt.grid(True)
 
 if FIRST_SIGNAL_SAMPLE_RATE != SECOND_SIGNAL_SAMPLE_RATE:
     print("SAMPLE RATE ERROR")
@@ -20,13 +33,13 @@ t_0_one = round(
 t_half_ATQA_one = round(
     1 / 847500 * 4 * FIRST_SIGNAL_SAMPLE_RATE)
 
-
 t_ATQA_two = round(
     1 / 847500 * 8 * SECOND_SIGNAL_SAMPLE_RATE * 18.5)
 t_0_two = round(
     1 / 847500 * 8 * SECOND_SIGNAL_SAMPLE_RATE)
 t_half_ATQA_two = round(
     1 / 847500 * 4 * SECOND_SIGNAL_SAMPLE_RATE)
+
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
@@ -43,120 +56,46 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=1):
 
 
 def get_sync_sample_position(first_signal_data: np.array, second_signal_data: np.array):
-    filtered_first_signal = butter_bandpass_filter(first_signal_data, 847500 - 847500 / 2, 847500 + 847500 / 4,
+    filtered_first_signal = butter_bandpass_filter(first_signal_data, 847500 - 847500 / 2, 847500 + 847500 / 1,
                                                    FIRST_SIGNAL_SAMPLE_RATE, 1)
-    filtered_second_signal = butter_bandpass_filter(second_signal_data, 847500 - 847500 / 2, 847500 + 847500 / 4,
+    filtered_second_signal = butter_bandpass_filter(second_signal_data, 847500 - 847500 / 2, 847500 + 847500 / 1,
                                                     SECOND_SIGNAL_SAMPLE_RATE, 1)
 
     first_ATQA_sample_position = 0
     second_ATQA_sample_posion = 0
 
-    if FAST_MODE:
-        __find_max_value_one = 0
-        for i in range(len(filtered_first_signal) - t_ATQA_one):
-            if __find_max_value_one < np.abs(filtered_first_signal[i:i + t_ATQA_one - 1]).sum():
-                first_ATQA_sample_position = i
-                __find_max_value_one = np.abs(filtered_first_signal[i:i + t_ATQA_one - 1]).sum()
+    __find_max_value_one = 0
+    __find_max_value_two = 0
 
-        __find_max_value_two = 0
-        for j in range(len(filtered_second_signal) - t_ATQA_two):
-            if __find_max_value_two < np.abs(filtered_second_signal[j:j + t_ATQA_two - 1]).sum():
-                second_ATQA_sample_posion = j
-                __find_max_value_two = np.abs(filtered_second_signal[j:j + t_ATQA_two - 1]).sum()
+    __value = 100000000
 
-    else:
-        __value = 9999999999999
-        __find_max_value_one = 0
-        for first_signal_sample in range(len(filtered_first_signal) - t_ATQA_one):
-            if __find_max_value_one < np.abs(filtered_first_signal[first_signal_sample:first_signal_sample + t_ATQA_one - 1]).sum():
-                first_ATQA_sample_position = first_signal_sample
-                __find_max_value_one = np.abs(filtered_first_signal[first_signal_sample:first_signal_sample + t_ATQA_one - 1]).sum()
+    for i in range(len(filtered_first_signal) - t_ATQA_one):
+        if __find_max_value_one < np.abs(filtered_first_signal[i:i + t_ATQA_one - 1]).sum():
+            first_ATQA_sample_position = i
+            __find_max_value_one = np.abs(filtered_first_signal[i:i + t_ATQA_one - 1]).sum()
 
-                for second_signal_sample in range(len(filtered_second_signal) - t_ATQA_two):
-                        a = (np.abs(filtered_first_signal[first_signal_sample:first_signal_sample + t_ATQA_one - 1]) -
-                             np.abs(filtered_second_signal[second_signal_sample:second_signal_sample + t_ATQA_two - 1])
-                             ).sum()
-                        if __value > a:
-                            __value = a
-                            second_ATQA_sample_posion = second_signal_sample
-                            print('...')
+    for j in range(len(filtered_second_signal) - t_ATQA_two):
+        if __find_max_value_two < np.abs(filtered_second_signal[j:j + t_ATQA_two - 1]).sum():
+            __find_max_value_two = np.abs(filtered_second_signal[j:j + t_ATQA_two - 1]).sum()
 
+    __ratio = __find_max_value_one / __find_max_value_two
+
+    filtered_second_signal = filtered_second_signal * __ratio
+
+    for second_signal_sample in range(len(filtered_second_signal) - t_ATQA_two):
+        a = (filtered_first_signal[first_ATQA_sample_position:first_ATQA_sample_position + t_ATQA_one - 1] -
+             filtered_second_signal[second_signal_sample:second_signal_sample + t_ATQA_two - 1]
+             ).std()
+        if __value > a:
+            __value = a
+            second_ATQA_sample_posion = second_signal_sample
+            print(a)
+
+    plt.figure(4)
+    show_plot(filtered_first_signal[first_ATQA_sample_position - 20:],
+              filtered_second_signal[second_ATQA_sample_posion - 20:])
     print(first_ATQA_sample_position, second_ATQA_sample_posion)
     return first_ATQA_sample_position, second_ATQA_sample_posion
-
-# def get944microsec(data_one: np.array, data_two: np.array):
-#     find_max_value_one = 0
-#     first_ATQA_sample_one = 0
-#     for i in range(len(data_one) - t_ATQA_one):
-#         if find_max_value_one < data_one[i:i + t_ATQA_one].sum():
-#             first_ATQA_sample_one = i
-#             find_max_value_one = data_one[i:i + t_ATQA_one].sum()
-#
-#     data_one = data_one[first_ATQA_sample_one - t_0_one:first_ATQA_sample_one + t_ATQA_one + t_0_one]
-#
-#     a_one = data_two[:round(t_0_one / 2)]
-#     b_one = data_two[round(t_0_one / 2) + t_ATQA_one + t_0_one:]
-#
-#     lean_one = (np.mean(b_one) - np.mean(a_one) / t_ATQA_one)
-#
-#     for ii in range(len(data_one)):
-#         data_one[ii] + ii * lean_one
-#
-#     find_max_value_two = 0
-#     first_ATQA_sample_two = 0
-#     for j in range(len(data_two) - t_ATQA_two):
-#         if find_max_value_two < data_two[j:j + t_ATQA_two].sum():
-#             first_ATQA_sample_two = j
-#             find_max_value_two = data_two[j:j + t_ATQA_two].sum()
-#
-#     data_two = data_two[first_ATQA_sample_two - t_0_two:first_ATQA_sample_two + t_ATQA_two + t_0_two]
-#
-#     a_two = data_two[:round(t_0_two / 2)]
-#     b_two = data_two[round(t_0_two / 2) + t_ATQA_two + t_0_two:]
-#
-#     lean_two = round((np.mean(b_two) - np.mean(a_two)) / t_ATQA_two)
-#
-#     for jj in range(len(data_two)):
-#         data_two[jj] + jj * lean_two
-#
-#     print(len(data_one))
-#     print(len(data_two))
-#
-#     _min_sync = np.mean(b_one) - np.mean(b_two)
-#     data_two = data_two + _min_sync
-#     _max_ratio = np.max(data_one) / np.max(data_two)
-#     data_two = data_two * _max_ratio
-#
-#     find_sync_sample = 0
-#     find_min_value = find_max_value_one
-#     for k in range(t_0_two * 2):
-#         x = data_two[k + t_0_two:k + t_ATQA_two] - data_one[t_0_one:t_ATQA_one]
-#         if find_min_value > sum(np.abs(x)):
-#             find_sync_sample = k
-#             find_min_value = sum(np.abs(x))
-#
-#     data_two = data_two[find_sync_sample:]
-#     if find_sync_sample == 0:
-#         find_min_value = find_max_value_two
-#         for k in range(t_0_one * 2):
-#             x = data_one[k + t_0_one:k + t_ATQA_one] - data_two[t_0_one:t_ATQA_two]
-#             if find_min_value > sum(np.abs(x)):
-#                 find_sync_sample = k
-#                 find_min_value = sum(np.abs(x))
-#
-#         data_one = data_one[find_sync_sample:]
-#
-#     print(find_sync_sample)
-#
-#     # find_sink_value: float = 99999999999999999
-#     # sample_sink = 0
-#     # for k in range(round(sample_one/2)):
-#     #     a = data_one[t_0_one:sample_one] - data_one[k:round(sample_one / 2) + k]
-#     #     if find_sink_value > abs(a).sum():
-#     #         find_sink_value = abs(a).sum()
-#     #         sample_sink = k
-#
-#     return data_one, data_two
 
 
 filtered_first_signal = butter_bandpass_filter(FIRST_ORIGINAL_SIGNAL, 847500 - 847500 / 2, 847500 + 847500 / 2,
@@ -166,21 +105,43 @@ filtered_second_signal = butter_bandpass_filter(SECOND_ORIGINAL_SIGNAL, 847500 -
 
 sync_first_sample, sync_second_sample = get_sync_sample_position(FIRST_ORIGINAL_SIGNAL, SECOND_ORIGINAL_SIGNAL)
 
-#TODO: 노멀라이즈 함수 손좀 봐...
-def get_normalized_signal(first_signal: np.array, second_signal: np.array, first_sink: int, second_sink: int):
-    x2 = 1 + (np.abs(first_signal[first_sink:first_sink + 6 - 1]) -
-         np.abs(second_signal[second_sink:second_sink + 6 -1])
-         ).mean() / np.abs(second_signal[second_sink:second_sink + 6 -1]).mean()
-    return first_signal[first_sink - 10:], second_signal[second_sink - 10:] * x2
+
+def get_envelop_signal(first_signal: np.array, second_signal: np.array, first_sink: int, second_sink: int):
+    first_signal = get_envelop(first_signal)
+    second_signal = get_envelop(second_signal)
+    __ratio = first_signal[first_sink:first_sink + round(t_half_ATQA_one)].max() / second_signal[
+                                                                                       second_sink:second_sink + round(
+                                                                                           t_half_ATQA_two)].max()
+    second_signal = second_signal * __ratio
+
+    return first_signal[first_sink - 20:first_sink + t_half_ATQA_one + 20], second_signal[
+                                                                            second_sink - 20:second_sink + t_half_ATQA_two + 20]
 
 
-def show_plot(first_signal, second_signal):
-    plt.plot(first_signal, color='red')
-    plt.plot(second_signal, color='blue')
-    plt.axis('tight')
-    plt.xlabel('samples')
-    plt.ylabel('amplitude')
-    plt.grid(True)
+def default_normalized_signal(original_first_signal: np.array, original_second_signal: np.array, accuracy=1000):
+    __first_sink, __second_sink = get_sync_sample_position(original_first_signal, original_second_signal)
+    filtered_first_signal = butter_bandpass_filter(original_first_signal, 847500 - 847500 / 2, 847500 + 847500 / 1,
+                                                   FIRST_SIGNAL_SAMPLE_RATE, 1)
+    filtered_second_signal = butter_bandpass_filter(original_second_signal, 847500 - 847500 / 2, 847500 + 847500 / 1,
+                                                    SECOND_SIGNAL_SAMPLE_RATE, 1)
+
+    __ratio = 0
+    __find_min_value = 999999999
+    for amp in range(accuracy):
+        if __find_min_value > (filtered_first_signal[__first_sink:__first_sink + t_ATQA_one] - filtered_second_signal[
+                                                                                               __second_sink:__second_sink + t_ATQA_two] * 10 * amp / accuracy).std():
+            __find_min_value = (filtered_first_signal[__first_sink:__first_sink + t_ATQA_one] - filtered_second_signal[
+                                                                                                __second_sink:__second_sink + t_ATQA_two] * 10 * amp / accuracy).std()
+            __ratio = 10 * amp / accuracy
+
+    plt.figure(3)
+    show_plot(filtered_first_signal[__first_sink - 20:], filtered_second_signal[__second_sink - 20:] * __ratio)
+    print('ratio' + str(__ratio))
+
+    plt.figure(6)
+    show_plot(get_envelop(filtered_first_signal[__first_sink - 20:]),
+              get_envelop(filtered_second_signal[__second_sink - 20:] * __ratio))
+    return filtered_first_signal[__first_sink:], filtered_second_signal[__second_sink:] * __ratio
 
 
 plt.figure(1)
@@ -190,6 +151,10 @@ show_plot(filtered_first_signal[sync_first_sample:], filtered_second_signal[sync
 show_plot(FIRST_ORIGINAL_SIGNAL[sync_first_sample:], SECOND_ORIGINAL_SIGNAL[sync_second_sample:])
 
 plt.figure(2)
-ns1,ns2 = get_normalized_signal(filtered_first_signal,filtered_second_signal,sync_first_sample,sync_second_sample)
+ns1, ns2 = get_envelop_signal(filtered_first_signal, filtered_second_signal, sync_first_sample, sync_second_sample)
 show_plot(ns1, ns2)
+
+
+default_normalized_signal(FIRST_ORIGINAL_SIGNAL, SECOND_ORIGINAL_SIGNAL, 1000)
+
 plt.show()
