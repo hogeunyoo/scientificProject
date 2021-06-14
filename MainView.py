e59b1d7b0b115e79e1d64ee327177cc148162011
Hogeun Yoo <ho93un+git@gmail.com>
2021-06-14 17:47:19 +0900

    임시저장

---
+++ MainView.py
@@ -0,0 +1,17 @@
+import sys
+from scipy.io import wavfile
+
+from SignalModel import SignalModel
+from Tensorflow import Tensorflow
+from MainViewController import MainViewController
+
+class MainView:
+    def __init__(self):
+        pass
+
+    def firstView(self):
+        pass
+
+    def show_plot(self, signal_data):
+        pass
+
--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-import sys
-from scipy.io import wavfile
 
-from SignalModel import SignalModel
 from MainViewController import MainViewController
 
-
-FIRST_SIGNAL_SAMPLE_RATE, FIRST_ORIGINAL_SIGNAL = wavfile.read('./wavFiles/ho_id_1.wav')
-SECOND_SIGNAL_SAMPLE_RATE, SECOND_ORIGINAL_SIGNAL = wavfile.read('./wavFiles/ok_id_0.wav')
-
-
-class MainView:
-    def __init__(self):
-        self.commend = ''
-        self.mainViewController = MainViewController()
-        self.signalModel = SignalModel()
-
-    def firstView(self):
-        print('welcom')
-        a = str(sys.stdin.readline())
-        print(a)
-        if a == 'start\n':
-            print("temp_start")
-            self.mainViewController.temp_start()
-        elif a == 'temp_start':
-            print("pass")
-        else:
-            print("?")
-
-
-    def show_plot(self, signal_data):
-        pass
-
-
-mainView = MainView()
-mainView.firstView()
+start = MainViewController()
